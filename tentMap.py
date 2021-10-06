42270c458ca0edc6cf0f49c38de9a810ad2fd886
Adam Farmer <adam.farmer@mail.mcgill.ca>
2021-10-06 11:37:43 -0400

    Updated logistic/tent maps to save to file

--- tentMap.py
+++ tentMap.py
@@ -7,8 +7,8 @@ import mpl_scatter_density
 
 # Initialize starting values and output arrays
 mu = 1
-nXiters = 20
-muScaler = 0.001
+nXiters = 150
+muScaler = 0.0002
 muVals = []
 xVals = []
 
@@ -17,16 +17,18 @@ while mu <= 2:
         x = 0.5  
         for xIter in range(0,nXiters):
             x = mu*min(x, 1-x)
-            muVals.append(r)
+            muVals.append(mu)
             xVals.append(x)
         mu += muScaler    
     
 #plot the map
 colorscale = np.divide(np.array(muVals), 2)
-figure = plt.figure()
+figure = plt.figure(figsize=(11, 8.5), tight_layout=True)
 plot1 = figure.add_subplot(1,1,1, projection="scatter_density")
 #plot1.scatter_density(rvals, xvals, dpi=50, downres_factor=1, cmap="Blues")
-plot1.scatter(muVals, xVals, s=0.5, c=colorscale, cmap="coolwarm")
+plot1.scatter(muVals, xVals, s=0.001, c=colorscale, cmap="viridis")
 plt.xlabel("Mu (resolution of "+str(muScaler)+")")
 plt.ylabel("X (Iterated "+str(nXiters)+" times per Mu value)")
-plt.show()
+plt.title("Tent Map (Full)")
+plt.savefig("tentMap.png", orientation="landscape", bbox_inches="tight", dpi=200)
+#plt.show()
