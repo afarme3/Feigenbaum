d52cd0e861450bfbdda04c85da2feb87eb89cae3
Adam Farmer <adam.farmer@mail.mcgill.ca>
2021-11-14 22:02:36 -0500

    Added mandelbrot.py

---
+++ mandelbrot.py
@@ -0,0 +1,13 @@
+#mandelbrot.py
+#A script containing mandelbrot and hyperbrot objects, with rendering and plotting methods. 
+import numpy as np
+
+class mandelbrot:
+    def __init__(self, name, resolution):
+        self.name = name
+        self.resolution = resolution
+
+    #Function to render mandelbrot set with resolution self.resolution and 
+    #def render(self, a):
+    
+
