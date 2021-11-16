f91a7f63ba14394e3ede849deaa6a471e8869c2a
Adam Farmer <adam.farmer@mail.mcgill.ca>
2021-11-15 21:11:11 -0500

    Fixed Mandelbrot.py, rendering and plotting works now

--- mandelbrot.py
+++ mandelbrot.py
@@ -5,67 +5,67 @@ import matplotlib.pyplot as plt
 import cmath
 
 class mandelbrot:
-    def __init__(self, name):
-        self.name = name
+    def __init__(self, resolution):
+        self.resolution = resolution
 
-    #Function to render mandelbrot set with pixel width "resolution" and exponent "a"
-    def render(self, a, precision, resolution):
-        #Initialize center points. X is the real axis and Y is the complex axis.
-        x = 0
-        y = 0
+    #Function to render mandelbrot set with pixel width/height "resolution"^2 and exponent "a"
+    def render(self, a, precision=None, xRange=2, yRange=2, xCenter=0, yCenter=0):
+        #Initialize precision default if no value is supplied
+        if precision is None:
+            precision = round(self.resolution/2)
 
         #Initialize constants for later plotting
-        xRange = 6
-        yRange = 4
-        height = round(resolution/yRange)
-        minX = x-xRange/2
-        maxX = x+xRange/2
-        minY = y-yRange/2
-        maxY = y+yRange/2
+        minX = xCenter-xRange
+        maxX = xCenter+xRange
+        minY = yCenter-yRange
+        maxY = yCenter+yRange
 
         #Initialize output array. One row for real values, one row for complex values, one row for colormap. 
-        output = [[],[],[]]
+        output = [[],[],[],[]]
 
         #Iterate through pixels and run escape time algorithm
-        for row in range(height):
-            for coln in range(resolution):
+        for row in range(self.resolution):
+            for coln in range(self.resolution):
                 #Get coordinates for real and complex coordinates and add to output array
-                x = minX + coln * xRange/resolution
-                y = minY + row * yRange/resolution
+                x = minX + coln*(2*xRange)/self.resolution
+                y = minY + row*(2*yRange)/self.resolution
                 output[0].append(x)
                 output[1].append(y)
                 hasEscaped = False
 
                 #Iterate the point to escape
-                for i in range(1,precision+1):
-                    z = complex(x, y)
-                    z = z**a
-                    x = z.real  
-                    y = z.imag
-
+                c = complex(x, y)
+                z = 0
+                for i in range(1, precision):
+                    z = (z**a)+c
+                    
                     #Check if the point has escaped and add the number of iterations needed to do so
-                    if x*x + y*y > 4:
-                        output[2].append(i)
+                    if ((z.real**2)+(z.imag**2)) > 4:
+                        output[2].append(1/i)
+                        output[3].append((1-(1/i))**2)
                         hasEscaped = True
                         break
+                    
                 #If the point has escaped, set colormap index to 0
                 if hasEscaped == False:
-                    output[2].append(0)
+                    output[2].append(1)
+                    output[3].append(1)
 
         
         self.points = output
     
     #Function to use matplot lib to plot the values
-    def plot(self):
+    def plot(self, colorMap="YlOrRd"):
         matplot = plt.figure()
         ax = matplot.add_subplot()
 
         #Create scatter plot
-        ax.scatter(self.points[0], self.points[1], s=0.01, c=self.points[2], cmap="winter")
+        ax.scatter(self.points[0], self.points[1], s=100/self.resolution, c=self.points[2], alpha=self.points[3], cmap=colorMap)
         plt.show()
 
-test = mandelbrot("test")
-test.render(2, 500, 500)
+test = mandelbrot(300)
+test.render(3, precision=2000, yCenter=1, xRange=0.25, yRange=0.25)
+#print(np.unique(test.points[3]))
 test.plot()
         
 
