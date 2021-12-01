18c88ff9d38b220dcfb42fee34cde48b4bfaf09a
Adam Farmer <adam.farmer@mail.mcgill.ca>
2021-11-30 22:28:26 -0500

    Added gauss.py with classes for rendering gauss iterative maps

---
+++ gauss.py
@@ -0,0 +1,87 @@
+# gauss.py - a script to render gauss iterative map bifurcation diagrams
+import matplotlib.pyplot as plt
+import math
+import numpy as np
+
+class gauss:
+    def __init__(self, bresolution, a):
+        self.bRes = bresolution
+        self.alpha = a
+
+    def render(self, nIterations=100, InitialCondition=0):
+        self.nIters = nIterations
+        b = -1
+        x = InitialCondition
+        output = [[],[]]
+        while b <= 1:
+            for i in range(0,self.nIters):
+                x = math.exp((-self.alpha*(x**2))) + b
+                output[0].append(x)
+                output[1].append(b)
+                b += self.bRes
+        self.points = output
+
+    def plot(self, colormap="autumn", pointSize=0.1):
+
+        #Initialize matplotlib plot
+        matplot = plt.figure()
+        ax = matplot.add_subplot()
+
+        #Plot values
+        ax.scatter(self.points[1], self.points[0], s=pointSize, c=self.points[1], cmap=colormap)
+        ax.set_xlabel("Beta (Resolution of "+str(self.bRes)+")")
+        ax.set_ylabel("X (Iterated "+str(self.nIters)+" times per beta")
+        plt.title("Gauss Iterated Map with alpha="+str(self.alpha))
+
+        return matplot
+
+class multiGauss:
+    def __init__(self, aresolution, bresolution, aMin=3, aMax=7):
+        self.aRes = aresolution
+        self.bRes = bresolution
+        self.aMin = aMin
+        self.aMax = aMax
+
+    def render(self, nIterations=100, InitialCondition=0):
+        self.nIters = nIterations
+        a = self.aMin
+        output = [[],[],[]]
+
+        while a < self.aMax:
+            b = -1
+            x = InitialCondition
+
+            while b <= 1:
+                for i in range(0,self.nIters):
+                    x = math.exp((-a*(x**2))) + b
+                    output[0].append(x)
+                    output[1].append(b)
+                    output[2].append(a)
+                    b += self.bRes
+            a += self.aRes
+        self.points = output
+
+    def plot(self, colormap="autumn", pointSize=0.1):
+
+        #Initialize matplotlib plot
+        matplot = plt.figure()
+        ax = matplot.add_subplot(projection="3d")
+
+        #Plot values
+        ax.scatter(self.points[1], self.points[2], self.points[0], s=pointSize, c=self.points[1], cmap=colormap)
+        ax.set_xlabel("Beta (Resolution of "+str(self.bRes)+")")
+        ax.set_ylabel("X (Iterated "+str(self.nIters)+" times per beta")
+        ax.set_zlabel("Alpha (Resolution of "+str(self.aRes)+")")
+        plt.title("3d Gauss Map")
+
+        return matplot
+
+#test = multiGauss(aresolution=0.1, bresolution=0.01, aMin=2, aMax=8)
+#test.render()
+#test.plot(colormap="winter")
+#plt.show()
+
+test2 = gauss(bresolution=0.001, a=7)
+test2.render(nIterations=250)
+test2.plot()
+plt.show()
